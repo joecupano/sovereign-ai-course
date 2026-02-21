21a99513cee780193e6bc3edfaee5a4aa932f0f0
Joe Cupano <joe@cupano.com>
2026-02-21 02:22:11 -0500

    update

--- build-sovereign-ai-lab.sh
+++ build-sovereign-ai-lab.sh
@@ -9,14 +9,21 @@
 # 20210220 - 2300
 #
 
-BANNER_COLOR="\e[0;104m\e[K"   # blue
+INSTALL_COLOR="\e[0;104m\e[K"   # blue
+SUCCESS_COLOR="\e[0;42m\e[K"   # green
 BANNER_RESET="\e[0m"
 
+echo " "
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} Build Sovereign AI Lab"
+echo -e "${INSTALL_COLOR}"
+echo -e "${BANNER_RESET}"
+echo " "
 
 cat <<'EOF'
 
-With Ubuntu fresh install, this script sets up the environment performing
-the following in order:
+Given a fresh Ubuntu 24.04 install, this script sets up the Sovereign AI Lab
+environment performing the following in order:
 
 1. Update Repositories
 2. Install Ubuntu Drivers
@@ -27,49 +34,54 @@ the following in order:
 7. Pull an LLM (IBM Granite 3.3 8B)
 
 Starting ...
+
 EOF
 
+echo "Press any key to continue or Ctrl-C to exit..."
+read -n 1 -s -r
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 1. Update Repositories"
-echo -e "${BANNER_COLOR}"
+
+echo " "
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 1. Update Repositories"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"
 echo " "
 sudo apt update && sudo apt upgrade -y
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 2, Install Ubuntu Drivers"
-echo -e "${BANNER_COLOR}"
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 2, Install Ubuntu Drivers"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"
 echo " "
 sudo ubuntu-drivers autoinstall
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 3. Install NVIDIA Drivers and Utilities$"
-echo -e "${BANNER_COLOR}"
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 3. Install NVIDIA Drivers and Utilities"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"
 echo " "
 sudo apt install nvidia-utils-565-server nvtop btop -y
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 4. Install Build Utilities"
-echo -e "${BANNER_COLOR}"
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 4. Install Build Utilities"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"
 echo " "
 sudo apt install build-essential git gcc cmake curl -y
 
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 5. Install Python Environment and Development Utilities$"
-echo -e "${BANNER_COLOR}"
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 5. Install Python Environment and Development Utilities"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"   
 echo " "
 sudo apt install python3-pip python3-venv python3-dev -y
 
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 6. Install Ollama$"
-echo -e "${BANNER_COLOR}"
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 6. Install Ollama"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"
 echo " "
 curl -fsSL https://ollama.com/install.sh | sh
@@ -77,16 +89,19 @@ curl -fsSL https://ollama.com/install.sh | sh
 #systemctl status ollama
 
 echo " "
-echo -e "${BANNER_COLOR}"
-echo -e "${BANNER_COLOR} 7. Pull an LLM (IBM Granite 3.3 8B)$"
-echo -e "${BANNER_COLOR}"
+echo -e "${INSTALL_COLOR}"
+echo -e "${INSTALL_COLOR} 7. Pull an LLM (IBM Granite 3.3 8B)"
+echo -e "${INSTALL_COLOR}"
 echo -e "${BANNER_RESET}"   
 echo " "
 ollama pull granite3.3:8b
 
+echo " "
+echo -e "${SUCCESS_COLOR}                                                        "
+
 cat <<'EOF'
 
-With evertything installed, you can now run the LLM using
+With everything installed, you can now run the LLM using
 the following command to enter a chat promp session with the model:"
 
 ollama run granite3.3:8b
@@ -94,6 +109,10 @@ ollama run granite3.3:8b
 Enjoy !
 
 EOF
+
+echo -e "${SUCCESS_COLOR}                                                        "
+echo -e "${BANNER_RESET}"
+echo " "
 
 
 
