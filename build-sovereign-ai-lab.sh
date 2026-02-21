7e0d2d5d7a70bee442f98947ae1d415755972338
joecupano <joe@cupano.com>
2026-02-21 01:17:47 -0500

    update

--- build-sovereign-ai-lab.sh
+++ build-sovereign-ai-lab.sh
@@ -9,24 +9,53 @@
 # 20210220 - 2300
 #
 
-# 1. Update Repositories, Install Drivers, and Build Utilities
+cat <<'EOF'
+
+With Ubuntu fresh install, this script sets up the environment performing
+the following in order:
+
+1. Update Repositories, Install Drivers, and Build Utilities
+2. Install Python Environment and Development Utilities
+3. Install Ollama
+4. Pull an LLM (IBM Granite 3.3 8B)
+
+Starting ...
+EOF
+echo " "
+echo "1. Update Repositories, Install Drivers, and Build Utilities"
+echo " "
 sudo apt update && sudo apt upgrade -y
 sudo ubuntu-drivers autoinstall
 sudo apt install nvidia-utils-565-server nvtop btop -y
 sudo apt install build-essential git gcc cmake curl -y
 
-# 2. Install Python Environment and Development Utilities
+echo " "
+echo "2. Install Python Environment and Development Utilities"
+echo " "
 sudo apt install python3-pip python3-venv python3-dev -y
-#sudo reboot
 
-# 3. Install Ollama
+echo " "
+echo "3. Install Ollama"
+echo " "
 curl -fsSL https://ollama.com/install.sh | sh
 
-# 4. Verify Installation
 #systemctl status ollama
 
-# 5. Pull an LLM (IBM Granite 3.3 8B)
+echo " "
+echo "4. Pull an LLM (IBM Granite 3.3 8B)"
+echo " "
 ollama pull granite3.3:8b
 
-# 6. Run the LLM
-# ollama run granite3.3:8b
+cat <<'EOF'
+
+With evertything installed, you can now run the LLM using
+the following command to enter a chat promp session with the model:"
+
+ollama run granite3.3:8b
+
+Enjoy !
+
+EOF
+
+
+
